bc1403d189ca347972d5b93b0a21479acd424c70
Joseph Burnett <josephburnett79@gmail.com>
2017-03-23 04:24:28 +0000

    Basic parser. Does not read lists correctly.

--- src/callow.c
+++ src/callow.c
@@ -1,26 +1,134 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 
-typedef uint64_t symbol;
+typedef enum { LIST, SYMBOL, NUMBER, FUNC } type;
+
+typedef int64_t value;
 typedef struct {
-    car symbol;
-    cdr symbol;
+    type t;
+    value car;
+    value cdr;
 } cell;
 
-cell* new_cell(symbol car, symbol cdr)
+cell* new_cell(type t, value car, value cdr)
 {
     cell* c = malloc(sizeof(cell));
+    c->t = t;
     c->car = car;
     c->cdr = cdr;
     return c;
 }
 
-cell* NIL = new_cell(0, 0);
-cell* T =   new_cell((symbol) {'T',' ',' ',' ',' ',' ',' ',' '}, NIL);
+value NIL = 0;
 
-symbol LIST =   (symbol) {'L','I','S','T',' ',' ',' ',' '};
-symbol SYMBOL = (symbol) {'S','Y','M','B','O','L',' ',' '};
-symbol STRING = (symbol) {'S','T','R','I','N','G',' ',' '};
+cell* read(FILE *fp);
+
+cell* read_cell(FILE *fp)
+{
+    cell* l = read(fp);
+    l->cdr = (value) read(fp);
+    return l;
+}
+
+cell* read_symbol(FILE *fp)
+{
+    cell* l = new_cell(SYMBOL, 0, NIL);
+    int i = 0;
+    int c;
+    while((c = getc(fp)) != EOF) {
+	if (c >= 97 && c <= 122) {
+	    if (i == 7) {
+		printf("Error parsing. Symbol is too long: %c\n", c);
+		exit(1);
+	    }
+	    ((char*) &(l->car))[i] = c;
+	    i++;
+	    continue;
+	}
+	switch (c) {
+	case ' ': case ')':
+	    ungetc(c, fp);
+	    return l;
+	default:
+	    printf("Error parsing. Invalid symbol character: %c\n", c);
+	    exit(1);
+	}
+    }
+}
+
+cell* read_number(FILE *fp)
+{
+    value num = 0;
+    int c;
+    while((c = getc(fp)) != EOF) {
+	if (c == ' ' || c == ')') {
+	    return new_cell(NUMBER, num, NIL);
+	}
+	if (c < 0 || c > 9) {
+	    printf("Error parsing. Invalid number: %c\n", c);
+	    exit(1);
+	}
+	num = num * 10 + ((int64_t) c - 48);
+    }
+}
+
+cell* read(FILE *fp)
+{
+    int c;
+    while ((c = getc(fp)) != EOF) {
+	if (c >= 97 && c <= 122) {
+	    ungetc(c, fp);
+	    return read_symbol(fp);
+	}
+	switch (c) {
+	case '(':
+	    return read_cell(fp);
+	case ')':
+	    return (cell*) NIL;
+	case ' ':
+	    continue;
+	default:
+	    printf("Error parsing. Invalid char: %c\n", c);
+	    exit(1);
+	}
+    }
+}
+
+void print(cell* c)
+{
+    printf("(");
+    if (c == (cell*) NIL) {
+	printf(")");
+	return;
+    }
+    switch (c->t) {
+    case LIST:
+	printf("<LIST>");
+	print((cell*) c->car);
+	if (c->cdr == NIL) {
+	    printf("<NIL>");
+	    break;
+	}
+	print((cell*) c->cdr);
+	break;
+    case SYMBOL: {
+	char* sym = (char*) &(c->car);
+	int i;
+	for (i = 0; i < 8; i++) {
+	    printf("%c", sym[i]);
+	}
+	break;
+    }
+    case NUMBER:
+	printf("%ld", c->car);
+	break;
+    case FUNC:
+	printf("<func>");
+	break;
+    }
+    printf(")");
+}
 
 int main(int argc, char *argv[])
 {
@@ -29,27 +137,14 @@ int main(int argc, char *argv[])
 	return 1;
     }
     FILE *fp;
-    if ((fp = fopen(*argv, "r")) == NULL) {
+    if ((fp = fopen("test.clw", "r")) == NULL) {
 	printf("callow: can't open %s\n", *argv);
 	return 1;
     }
+    cell* c = read(fp);
+    print(c);
+    printf("\n");
     fclose(fp);
     return 0;
 }
 
-cell* read_cell(FILE *fp)
-{
-    int c;
-    while ((c = getc(fp)) != EOF) {
-	switch (c) {
-	case '(':
-	    return new_cell(LIST, read_cell(fp));
-	case 'a':
-	    return new_cell(STRING, read_cell(fp));
-	case ')':
-	    return 
-	default:
-	    continue;
-	}
-    }
-}
