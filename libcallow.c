fe417b702bc564739d30e666bd594fb084bac992
Joseph Burnett <josephburnett79@gmail.com>
2017-04-11 05:14:53 +0000

    More simple parsing function.

--- libcallow.c
+++ libcallow.c
@@ -23,148 +23,130 @@ cell* new_cell(type t, value car, value cdr)
 
 value NIL = 0;
 
-cell* read(FILE *fp);
 
-cell* list(FILE *fp) {
-    cell *cl = new_cell(LIST, 0, NIL);
-    cl->car = (value) read(fp);
-    cl->cdr = (value) read(fp);
-    return cl;
-}
+typedef enum { R_WHITESPACE, R_SYMBOL, R_NUMBER } reading;
 
-cell* symbol(FILE *fp) {
-    cell* cl = new_cell(SYMBOL, 0, NIL);
-    int ch;
-    int i = 0;
-    while ((ch = getc(fp)) != EOF) {
-	switch (ch) {
-	case ' ':
-	    cl->cdr = (value) read(fp);
-	    return cl;
-	case ')':
-	    cl->cdr = NIL;
-	    return cl;
-	default:
-	    if (ch >= 97 && ch <= 122) {
-		if (i == 7) {
-		    printf("Error parsing. Symbol is too long: %c\n", ch);
-		    exit(1);
-		}
-		((char*) &(cl->car))[i] = ch;
-		i++;
-		continue;
-	    } else {
-		printf("Error parsing. Invalid symbol character: %c\n", ch);
-		exit(1);
-	    }
-	}
-    }
-}
-
-cell* string(FILE *fp) {
-    cell* head = new_cell(SYMBOL, 0, NIL);
-    cell* tail = head;
-    int ch;
-    while ((ch = getc(fp)) != EOF) {
-	if (ch == '"') {
-	    return new_cell(LIST, (value) head, NIL);
-	}
-	if (ch >= 0 && ch <= 127) {
-	    ((char*) &(tail->car))[0] = ch;
-	    tail->cdr = (value) new_cell(SYMBOL, 0, NIL);
-	    tail = (cell*) tail->cdr;
-	    continue;
-	}
-	printf("Error parsing. Invalid string character: %c\n", ch);
-	exit(1);
-    }
-    return (cell*) NIL;
-}
-
-cell* number(FILE *fp) {
-    int ch;
-    value v = 0;
-    value sign = 1;
-    while ((ch = getc(fp)) != EOF) {
-	switch (ch) {
-	case ' ':
-	    return new_cell(NUMBER, v * sign, (value) read(fp));
-	case ')':
-	    return new_cell(NUMBER, v * sign, NIL);
-	default:
-	    if (ch == '-') {
-		sign = -1;
-		continue;
-	    }
-	    if (ch >= 48 && ch <= 57) {
-		v = v * 10 + (ch - 48);
-	    } else {
-		printf("Error parsing. Invalid symbol character: %c\n", ch);
-		exit(1);
-	    }
-	}
-    }
-}
+cell* string(FILE *fp);
 
 cell* read(FILE *fp) {
     int ch;
+    int index = 0;
+    value v = 0;
+    value sign = 1;
+    reading rd = R_WHITESPACE;
     while ((ch = getc(fp)) != EOF) {
-	switch (ch) {
-	case '(':
-	    return list(fp);
-	case ')':
-	    return (cell*) NIL;
-	case '"':
-	    return string(fp);
-	case ' ':
-	    continue;
-	default:
-	    if (ch >= 97 && ch <= 122) {
-		ungetc(ch, fp);
-		return symbol(fp);
-	    }
-	    if ((ch >= 48 && ch <= 57) || ch == '-') {
-		ungetc(ch, fp);
-		return number(fp);
-	    } else {
-		printf("Error parsing. Invalid char: %c\n", ch);
-		exit(1);
-	    }
-	}
+        switch (ch) {
+        case ' ':
+            switch (rd) {
+            case R_WHITESPACE:
+                continue;
+            case R_SYMBOL:
+                return new_cell(SYMBOL, v, (value) read(fp));
+            case R_NUMBER:
+                return new_cell(NUMBER, v * sign, (value) read(fp));
+            }
+        case '(': {
+            cell* cl = new_cell(LIST, (value) read(fp), NIL);
+            cl->cdr = (value) read(fp);
+            return cl;
+        }
+        case ')':
+            switch (rd) {
+            case R_SYMBOL:
+                return new_cell(SYMBOL, v, NIL);
+            case R_NUMBER:
+                return new_cell(NUMBER, v * sign, NIL);
+            default:
+                return (cell*) NIL;
+            }
+        case '-':
+            switch (rd) {
+            case R_WHITESPACE:
+                sign = -1;
+                rd = R_NUMBER;
+                continue;
+            default:
+                printf("Error parsing. Invalid '-' (only allowed before numbers).\n");
+                exit(1);
+            }
+        case '0' ... '9':
+            switch (rd) {
+            case R_WHITESPACE:
+            case R_NUMBER:
+                rd = R_NUMBER;
+                ch = ch - '0';
+                v = v * 10 + ch;
+                continue;
+            default:
+                printf("Error parsing. Invalid character: %c\n", ch);
+                exit(1);
+            }
+        case 'a' ... 'z':
+            switch (rd) {
+            case R_WHITESPACE:
+            case R_SYMBOL:
+                rd = R_SYMBOL;
+                if (index == 7) {
+                    printf("Error parsing. Symbol is too long.\n");
+                    exit(1);
+                }
+                ((char *) &v)[index++] = ch;
+                continue;
+            }
+        case '"': {
+            cell* cl = new_cell(LIST, (value) string(fp), NIL);
+            cl->cdr = (value) read(fp);
+            return cl;
+        }
+        default:
+            exit(1);
+        }
     }
     return (cell*) NIL;
+}
+
+cell* string(FILE *fp) {
+    int ch;
+    value v = 0;
+    while ((ch = getc(fp)) != EOF) {
+        if (ch == '"') {
+            return (cell*) NIL;
+        } else {
+            ((char*) &v)[0] = ch;
+            return new_cell(SYMBOL, v, (value) string(fp));
+        }
+    }
 }
 
 void print(FILE *fp, cell* c, int index, int depth)
 {
     if (index > 0) {
-	fprintf(fp, " ");
+        fprintf(fp, " ");
     }
     switch (c->t) {
     case LIST:
-	fprintf(fp, "(");
-	print(fp, (cell*) c->car, 0, depth + 1);
-	fprintf(fp, ")");
-	break;
+        fprintf(fp, "(");
+        print(fp, (cell*) c->car, 0, depth + 1);
+        fprintf(fp, ")");
+        break;
     case SYMBOL: {
-	char* sym = (char*) &(c->car);
-	int i;
-	for (i = 0; i < 8; i++) {
-	    if (sym[i] != 0) {
-		fprintf(fp, "%c", sym[i]);
-	    }
-	}
-	break;
+        char* sym = (char*) &(c->car);
+        int i;
+        for (i = 0; i < 8; i++) {
+            if (sym[i] != 0) {
+                fprintf(fp, "%c", sym[i]);
+            }
+        }
+        break;
     }
     case NUMBER:
-	fprintf(fp, "%" PRId64, c->car);
-	break;
+        fprintf(fp, "%" PRId64, c->car);
+        break;
     case FUNC:
-	fprintf(fp, "<func>");
-	break;
+        fprintf(fp, "<func>");
+        break;
     }
     if (c->cdr != NIL) {
-	print(fp, (cell*) c->cdr, index + 1, depth);
+        print(fp, (cell*) c->cdr, index + 1, depth);
     }
 }
-
--- test.c
+++ test.c
@@ -4,59 +4,59 @@
 
 char* test_cases[][3] = {
     {
-    	"Single symbol list",
-    	"(abcd)",
-    	"(abcd)"
+        "Single symbol list",
+        "(abcd)",
+        "(abcd)"
     },
     {
-    	"Multiple symbol list",
-    	"(a b c d)",
-    	"(a b c d)"
-    },
-    {
-	"Nested lists",
-	"(a (b) ((c)))",
-	"(a (b) ((c)))"
-    },
-    {
-	"Nested lists",
-	"((a) (b))",
-	"((a) (b))"
-    },
-    {
-	"Whitespace ignored",
-	"( a  b     c)",
-	"(a b c)"
-    },
-    {
-	"Single number list",
-	"(1234)",
-	"(1234)"
-    },
-    {
-	"Multiple number list",
-	"(1 2 3 4)",
-	"(1 2 3 4)"
-    },
-    {
-	"Negative numbers",
-	"(-1 -2 -3)",
-	"(-1 -2 -3)"
-    },
-    {
-	"Single string",
-	"\"abcd\"",
+        "Multiple symbol list",
+        "(a b c d)",
         "(a b c d)"
     },
     {
-	"Single string list",
-	"(\"abcd\")",
-	"((a b c d))"
+        "Nested lists",
+        "(a (b) ((c)))",
+        "(a (b) ((c)))"
     },
     {
-	"Nested string lists",
-	"(\"a\" \"b\")",
-	"((a) (b))"
+        "Nested lists",
+        "((a) (b))",
+        "((a) (b))"
+    },
+    {
+        "Whitespace ignored",
+        "( a  b     c)",
+        "(a b c)"
+    },
+    {
+        "Single number list",
+        "(1234)",
+        "(1234)"
+    },
+    {
+        "Multiple number list",
+        "(1 2 3 4)",
+        "(1 2 3 4)"
+    },
+    {
+        "Negative numbers",
+        "(-1 -2 -3)",
+        "(-1 -2 -3)"
+    },
+    {
+        "Single string",
+        "\"abcd\"",
+        "(a b c d)"
+    },
+    {
+        "Single string list",
+        "(\"abcd\")",
+        "((a b c d))"
+    },
+    {
+        "Nested string lists",
+        "(\"a\" \"b\")",
+        "((a) (b))"
     }
 };
 
@@ -75,11 +75,11 @@ int check(char name[], char given[], char expect[])
     fclose(stream);
 
     if (strcmp(actual, expect) != 0) {
-	printf("\nFAIL: %s\n", name);
-	printf("    Given:    %s\n", given);
-	printf("    Expected: %s\n", expect);
-	printf("    Got:      %s\n", actual);
-	return 1;
+        printf("\nFAIL: %s\n", name);
+        printf("    Given:    %s\n", given);
+        printf("    Expected: %s\n", expect);
+        printf("    Got:      %s\n", actual);
+        return 1;
     }
     return 0;
 }
@@ -89,12 +89,12 @@ int main(int argc, char *argv[])
     int fail = 0;
     int i;
     for (i = 0; i < sizeof(test_cases) / sizeof(test_cases[0]); i++) {
-	fail += check(test_cases[i][0], test_cases[i][1], test_cases[i][2]);
+        fail += check(test_cases[i][0], test_cases[i][1], test_cases[i][2]);
     }
 
     if (fail == 0) {
-	printf("\nALL TESTS PASSED!\n\n");
+        printf("\nALL TESTS PASSED!\n\n");
     } else {
-	printf("\n%d FAILED TESTS!\n\n", fail);
+        printf("\n%d FAILED TESTS!\n\n", fail);
     }
 }
