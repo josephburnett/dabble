88bb711b01115e66b5721602de16222695e6fa4f
Joseph Burnett <josephburnett79@gmail.com>
2021-09-08 22:06:18 +0200

    Repl.

--- go/eval/eval.go
+++ go/eval/eval.go
@@ -6,26 +6,23 @@ import (
 )
 
 func Eval(env, value object.Value) object.Value {
-	if value == nil || value == object.Null {
-		return object.Null
-	}
 	switch value.Type() {
-	case object.NUMBER:
+	case object.NUMBER, object.NULL, object.ERROR:
 		return value
 	case object.SYMBOL:
 		r := resolve(env, value)
 		return Eval(env, r)
 	case object.CELL:
 		first := Eval(env, value.First())
-		if _, ok := first.(object.Error); ok {
+		if first.Type() == object.ERROR {
 			return first
 		}
 		rest := Eval(env, value.Rest())
-		if _, ok := rest.(object.Error); ok {
+		if rest.Type() == object.ERROR {
 			return rest
 		}
 		return object.Cell(first, rest)
 	default:
-		return object.Error(fmt.Sprintf("unknown type: %T", value))
+		return object.Error(fmt.Sprintf("eval: unknown type: %T", value))
 	}
 }
---
+++ go/repl/repl.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"dabble/eval"
+	"dabble/lexer"
+	"dabble/object"
+	"dabble/parser"
+	"fmt"
+	"io"
+	"os"
+	"os/user"
+)
+
+// Based on Monkey repl.go.
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Dabble programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	Start(os.Stdin, os.Stdout)
+}
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program, err := p.ParseProgram()
+		if err != nil {
+			io.WriteString(out, err.Error())
+			io.WriteString(out, "\n")
+			continue
+		}
+
+		evaluated := eval.Eval(object.Null, program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
