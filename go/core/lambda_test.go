e366cda349b75c70428896c708ae284d6a02e569
Joseph Burnett <josephburnett79@gmail.com>
2021-09-24 09:09:58 +0200

    Test lambda free variables.

--- go/core/lambda_test.go
+++ go/core/lambda_test.go
@@ -1,14 +1,32 @@
 package core
 
 import (
+	"dabble/eval"
 	"dabble/object"
+	"fmt"
 	"testing"
 )
 
 func TestLambda(t *testing.T) {
 
+	adder := func(env *object.Binding, args ...object.Value) object.Value {
+		if err := argsLenError("adder", args, 2); err != nil {
+			return err
+		}
+		first := eval.Eval(env, args[0])
+		if first.Type() != object.NUMBER {
+			return object.Error(fmt.Sprintf("not a number: %v", first))
+		}
+		second := eval.Eval(env, args[1])
+		if second.Type() != object.NUMBER {
+			return object.Error(fmt.Sprintf("not a number: %v", second))
+		}
+		return first.(object.Number) + second.(object.Number)
+	}
+
 	env := &object.Binding{"lambda", object.Function(Lambda),
-		&object.Binding{"quote", object.Function(Quote), nil}}
+		&object.Binding{"quote", object.Function(Quote),
+			&object.Binding{"+", object.Function(adder), nil}}}
 
 	tests := []coreTest{{
 		input: "((lambda () 1))",
@@ -18,6 +36,10 @@ func TestLambda(t *testing.T) {
 		input: "((lambda (a) a) 1)",
 		env:   env,
 		want:  "1",
+	}, {
+		input: "((lambda (a b) (+ a b)) 1 2)",
+		env:   env,
+		want:  "3",
 	}}
 
 	testCore(t, tests)
