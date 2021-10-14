f61a917e5c4f472b991b076f8ab215ff5d7806ff
Joseph Burnett <josephburnett79@gmail.com>
2021-10-14 13:22:13 +0200

    More and tests.

--- go/core/lib_test.go
+++ go/core/lib_test.go
@@ -42,8 +42,8 @@ func TestLib(t *testing.T) {
 			value := eval.Eval(Env, program)
 			trace := eval.EndTrace()
 			if value.Type() != object.SYMBOL || value.(object.Symbol) != "t" {
-				t.Errorf("%v", value)
 				t.Errorf("TRACE:\n%v", trace)
+				t.Errorf("%v", value)
 				return
 			}
 		})
