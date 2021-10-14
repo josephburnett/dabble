22236fd9a1404d2f37fe5fb7b4d5121fd01929d3
Joseph Burnett <josephburnett79@gmail.com>
2021-10-14 09:11:36 +0200

    Remove unnecessary recur evaluation.

--- go/core/recur.go
+++ go/core/recur.go
@@ -6,14 +6,6 @@ import (
 )
 
 func Recur(env *eval.Frame, args ...object.Value) object.Value {
-	// Is this necessary?
-	// for i, arg := range args {
-	// 	value := eval.Eval(env, arg)
-	// 	if value.Type() == object.ERROR {
-	// 		return value
-	// 	}
-	// 	args[i] = value
-	// }
 	lastCaller := env.LastCaller()
 	eval.T("recurring on %v", lastCaller)
 	return lastCaller.Fn(env, args...)
